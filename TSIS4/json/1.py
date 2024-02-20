4cf27a177bceaba48bba47b83d8a7b2553b26653
Alisher <alisher.baudinovv@gmail.com>
2024-02-20 21:45:45 +0600

    tsis4 finished

--- TSIS4/json/1.py
+++ TSIS4/json/1.py
@@ -3,5 +3,7 @@ import json
 file = open("sample-data.json")
 
 data_json = file.read()
-data = json.loads("data_json")
-print(data["totalCount"])
+data = json.loads(data_json)
+print("Interface Status \n" + "================================================================================\n DN                                                 Description           Speed    MTU  \n -------------------------------------------------- --------------------  ------  ------\n" + data["imdata"][0]["l1PhysIf"]["attributes"]["dn"] + "                              " + data["imdata"][0]["l1PhysIf"]["attributes"]["fecMode"] + " " + data["imdata"][0]["l1PhysIf"]["attributes"]["mtu"])
+print(data["imdata"][1]["l1PhysIf"]["attributes"]["dn"] + "                              " + data["imdata"][1]["l1PhysIf"]["attributes"]["fecMode"] + " " + data["imdata"][1]["l1PhysIf"]["attributes"]["mtu"])
+print(data["imdata"][2]["l1PhysIf"]["attributes"]["dn"] + "                              " + data["imdata"][2]["l1PhysIf"]["attributes"]["fecMode"] + " " + data["imdata"][1]["l1PhysIf"]["attributes"]["mtu"])
